f2e588da1d3f0fc5753ce2e230e83380d38bf813
Per Holte <phholte@stud.ntnu.no>
2021-10-18 23:20:44 +0200

    #14 - Added placeholder homepage for referance

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Homepage from './components/homepage/Homepage';
 
 function App() {
-    return <Header />;
+    return (
+        <div className="app">
+            <Header />
+            <Homepage />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Beer } from '../../beer.svg';
+
+const Homepage = () => {
+    return (
+        <div background-color="black">
+            <Beer />
+        </div>
+    );
+};
+
+export default Homepage;
