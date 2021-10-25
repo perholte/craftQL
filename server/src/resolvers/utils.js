72593788f8d6adf9fe3efa302928599406631e32
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2021-10-25 22:52:09 +0200

    \#18 - Utility function for mapping ordering objects

--- server/src/resolvers/utils.js
+++ server/src/resolvers/utils.js
@@ -1,6 +1,5 @@
 
 function parseBeer(beer) {
-    console.log(beer)
     return {
             id: beer["BeerID"], 
             brand: beer["Brand_BeerToBrand"]["Name"], 
@@ -11,4 +10,23 @@ function parseBeer(beer) {
     };
 }
 
-module.exports = parseBeer;
+function mapOrdering(ordering) {
+    let column = Object.keys(ordering)[0]
+    let ord = ordering[column]
+    switch (column) {
+        case 'brand':
+            return {Brand_BeerToBrand: { Name: ord}}
+        case 'type':
+            return {Type_BeerToType: {Name: ord}}
+        case 'name':
+            return {Name: ord}
+        case 'abv':
+            return {Volume: ord}
+        case 'rating':
+            return {Review: { groupBy: {}, _avg: ord }}
+        default:
+            return {Volume: ord}
+    }
+}
+
+module.exports = {parseBeer, mapOrdering};
