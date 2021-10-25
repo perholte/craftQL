fb19dca18828e95bde4951c3c64c8685f732fd75
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2021-10-25 22:02:57 +0200

    \#18 - Added filtering logic

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,8 +1,22 @@
 const parseBeer = require('./utils')
 
 async function beers(parent, args, context, info) {
-    return  (await context.prisma.beer.findMany({include: {Type_BeerToType: true, Brand_BeerToBrand: true, _count: true}}))
-        .map(parseBeer);
+    let { filter, skip, take } = args
+    const where = filter
+        ? {
+        OR: [
+            { Name: { contains: filter.toLowerCase().trim() } },
+            { Type_BeerToType: { Name: {contains: filter }} },
+            { Brand_BeerToBrand: { Name: {contains: filter }} },
+        ],
+        } : {}
+    return  (await context.prisma.beer.findMany(
+        {   
+            where, 
+            include: {Type_BeerToType: true, Brand_BeerToBrand: true, _count: true}, 
+            take: take || 15, 
+            skip 
+    })).map(parseBeer);
 }
 
 module.exports = {
