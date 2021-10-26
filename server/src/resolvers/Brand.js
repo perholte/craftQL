ad66256352df96c54f3c638c2efc494db70d9da4
Jørgen Reimers <jorgenreim@gmail.com>
2021-10-27 01:47:17 +0200

    Add resolvers for non-scalar types in Beer

---
+++ server/src/resolvers/Brand.js
@@ -0,0 +1,7 @@
+function brand(parent, args, context, info) {
+    return context.prisma.beer.findUnique({ where: { id: parentId } }).brand();
+}
+
+module.exports = {
+    brand,
+};
---
+++ server/src/resolvers/Type.js
@@ -0,0 +1,7 @@
+function type(parent, args, context, info) {
+    return context.prisma.beer.findUnique({ where: { id: parentId } }).type();
+}
+
+module.exports = {
+    type,
+};
