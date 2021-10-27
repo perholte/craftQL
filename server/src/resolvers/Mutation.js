7c25197baa551200107849e8818459230cdd5084
Jørgen Reimers <jorgenreim@gmail.com>
2021-10-27 13:47:15 +0200

    Validate input for mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -2,21 +2,27 @@ const { UserInputError } = require('apollo-server-errors');
 
 async function rateBeer(parent, args, context, info) {
     const { beerId, rating } = args;
+
+    if (rating < 1 || rating > 5) {
+        throw new UserInputError('Rating must be between 1 and 5');
+    }
+    console.log(parent);
+
     const beerIdInt = parseInt(beerId);
-    const beer = await context.prisma.beer.findUnique({
-        where: {
-            id: beerIdInt,
-        },
+    const beerExists = await context.prisma.$exists.beer({
+        id: beerIdInt,
     });
-    if (!beer) {
+    if (!beerExists) {
         throw new UserInputError('No beer with this id');
     }
+
     const newReview = await context.prisma.review.create({
         data: {
             beerId: beerIdInt,
             rating,
         },
     });
+
     return await context.prisma.beer.findUnique({
         where: {
             id: newReview.beerId,
