70f779be06c198edefc01dc44c6f878d5d5cb9c5
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2021-10-29 11:39:35 +0200

    #20 - Added query that sorts beers on average rating

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -13,6 +13,8 @@ async function beers(parent, args, context) {
           }
         : {};
 
+    const SORT_BY_RATING_QUERY =
+        'SELECT Beer.name, Beer.abv, Brand.name, Type.name, AVG(Review.rating) AS Rating FROM Beer JOIN Review ON Beer.id = Review.beerId JOIN Brand ON Beer.brandId = Brand.id JOIN Type ON Beer.typeId = Type.idGROUP BY Beer.id, Review.beerIdORDER BY Rating ASC;';
     return await context.prisma.beer.findMany({
         where,
         skip,
