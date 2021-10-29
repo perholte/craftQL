0bf5003231978ce5750d02537742fa5178e29582
Jørgen Reimers <jorgenreim@gmail.com>
2021-10-29 10:51:37 +0200

    #20 - Only have Beer in graphql schema

--- server/src/resolvers/Beer.js
+++ server/src/resolvers/Beer.js
@@ -1,9 +1,9 @@
 async function brand(parent, args, context) {
-    return await context.prisma.beer.findUnique({ where: { id: parent.id } }).brand();
+    return (await context.prisma.beer.findUnique({ where: { id: parent.id } }).brand()).name;
 }
 
-async function type(parent, args, context, info) {
-    return await context.prisma.beer.findUnique({ where: { id: parent.id } }).type();
+async function type(parent, args, context) {
+    return (await context.prisma.beer.findUnique({ where: { id: parent.id } }).type()).name;
 }
 
 async function rating(parent, args, context, info) {
