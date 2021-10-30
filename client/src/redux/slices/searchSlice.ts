db18f5e7a405fc3d4c3e8722638a9668ddc6ef45
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2021-10-31 01:10:51 +0200

    #25 - Updated SearchState and functions for it

--- client/src/redux/slices/searchSlice.ts
+++ client/src/redux/slices/searchSlice.ts
@@ -1,11 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+export enum SearchFilter {
+    name = 'name',
+    brand = 'brand',
+    type = 'type',
+    all = 'all',
+}
+
 export interface SearchState {
     value: string;
+    filter: SearchFilter;
 }
 
 const initialState: SearchState = {
     value: '',
+    filter: SearchFilter.name,
 };
 
 export const counterSlice = createSlice({
@@ -15,13 +24,17 @@ export const counterSlice = createSlice({
         setSearchValue: (state, action: PayloadAction<string>) => {
             state.value = action.payload;
         },
+        setSearchCategory: (state, action: PayloadAction<SearchFilter>) => {
+            state.filter = action.payload;
+        },
         clearSearch: (state) => {
             state.value = '';
+            state.filter = SearchFilter.name;
         },
     },
 });
 
 // Action creators are generated for each case reducer function
-export const { setSearchValue } = counterSlice.actions;
+export const { setSearchValue, setSearchCategory } = counterSlice.actions;
 
 export default counterSlice.reducer;
