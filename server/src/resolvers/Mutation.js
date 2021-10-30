7b86bbe63031b04ca49f9201aa303dc03c3bb8f3
André Schjøth <andschj@stud.ntnu.no>
2021-10-30 23:33:49 +0200

    #11 fix bug in mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,16 +1,23 @@
 const { UserInputError } = require('apollo-server-errors');
 
-async function rateBeer(args, context) {
+/**
+ *  Resolver for the rateUser endpoint
+ * @param {contains the user input} args
+ * @param {application context for accessing prisma} context
+ * @returns the beer with the id given by the user with an updated rating or null if the beer doesn't exist
+ */
+async function rateBeer(parent, args, context) {
     const { beerId, rating } = args;
-
     if (rating < 1 || rating > 5) {
         throw new UserInputError('Rating must be between 1 and 5');
     }
-
     const beerIdInt = parseInt(beerId);
-    const beerExists = await context.prisma.$exists.beer({
-        id: beerIdInt,
-    });
+    const beerExists =
+        (await context.prisma.beer.findUnique({
+            where: {
+                id: beerIdInt,
+            },
+        })) != undefined;
     if (!beerExists) {
         throw new UserInputError('No beer with this id');
     }
@@ -22,7 +29,7 @@ async function rateBeer(args, context) {
         },
     });
 
-    return await context.prisma.beer.findUnique({
+    return context.prisma.beer.findUnique({
         where: {
             id: newReview.beerId,
         },
