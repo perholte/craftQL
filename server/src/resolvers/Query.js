4088c623f871d94117f39da8b182558a0b97990b
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2021-10-31 01:05:52 +0200

    #25 - Utility function for sorting on beer rating

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,4 +1,4 @@
-const { parseOrderBy } = require('./utils');
+const { parseOrderBy, parseFilter } = require('./utils');
 
 /**
  *  Resolver for the beers endpoint
@@ -9,23 +9,56 @@ const { parseOrderBy } = require('./utils');
  */
 async function beers(parent, args, context) {
     const { filter, skip, take, orderBy } = args;
+    const where = parseFilter(filter);
+    if (Object.keys(orderBy).includes("rating")) {
+        return beersSortedByRating(context.prisma, args);
+    } else {
+        return context.prisma.beer.findMany({
+            where,
+            skip,
+            take,
+            orderBy: orderBy ? parseOrderBy(orderBy) : undefined,
+        });
+    }
+}
 
-    const where = filter
-        ? {
-              OR: [
-                  { name: { contains: filter } },
-                  { brand: { name: { contains: filter } } },
-                  { type: { name: { contains: filter } } },
-              ],
-          }
-        : {};
-
-    return await context.prisma.beer.findMany({
-        where,
-        skip,
-        take,
-        orderBy: orderBy ? parseOrderBy(orderBy) : undefined,
-    });
+/**
+ * We need this method because the rating for each beer is an aggregated value 
+ * from the Review table. This function performs joins on the Beer, Review, Brand 
+ * and Type tables. Then groups each beer with its associated reviews, and lastly 
+ * aggregates the average value for rating.  
+ * 
+ * @param {*} prisma 
+ * @param {*} param1 
+ * @returns 
+ */
+async function beersSortedByRating(prisma, { filter, skip, take, orderBy }) {
+    // filter = filter ? filter : Prisma.empty;
+    skip = skip ? skip : 0;
+    take = take ? take : 5;
+    if (orderBy.rating && orderBy.rating === "desc") {
+        return prisma.$queryRaw`
+            SELECT Beer.id AS id, Beer.name AS name, Beer.abv, Brand.name AS brand, Type.name AS type, AVG(Review.rating) AS rating
+                FROM Beer
+                    JOIN Review ON Beer.id = Review.beerId
+                    JOIN Brand ON Beer.brandId = Brand.id
+                    JOIN Type ON Beer.typeId = Type.id
+                    GROUP BY Beer.id, Review.beerId
+                    ORDER BY Rating DESC
+                    LIMIT ${take}
+                    OFFSET ${skip};`;
+    } else {
+        return prisma.$queryRaw`
+            SELECT Beer.id AS id, Beer.name AS name, Beer.abv, Brand.name AS brand, Type.name AS type, AVG(Review.rating) AS rating
+                FROM Beer
+                    JOIN Review ON Beer.id = Review.beerId
+                    JOIN Brand ON Beer.brandId = Brand.id
+                    JOIN Type ON Beer.typeId = Type.id
+                    GROUP BY Beer.id, Review.beerId
+                    ORDER BY Rating ASC
+                    LIMIT ${take}
+                    OFFSET ${skip};`;
+    }
 }
 
 module.exports = {
